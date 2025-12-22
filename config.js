c9ef9d579716772c8dd7a6dd949c1a1a0e437a6a
sumammelinamath-hash <sumammelinamath@gmail.com>
2025-12-22 16:06:21 +0530

    config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  token: process.env.TOKEN,
+  mongoURI: process.env.MONGO_URI,
+  adminRoleID: process.env.ADMIN_ROLE_ID,
+  adminChannelID: process.env.ADMIN_CHANNEL_ID,
+  logChannelID: process.env.LOG_CHANNEL_ID
+};
