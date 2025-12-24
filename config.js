6400a0363596e436982642401b6a88a879a8a8bb
Shreyas <sumammelinamath@gmail.com>
2025-12-24 20:24:20 +0530

    config.js

--- config.js
+++ config.js
@@ -3,5 +3,13 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   adminRoleID: process.env.ADMIN_ROLE_ID,
   adminChannelID: process.env.ADMIN_CHANNEL_ID,
-  logChannelID: process.env.LOG_CHANNEL_ID
+  logChannelID: process.env.LOG_CHANNEL_ID,
+
+  brand: {
+    name: "MineCom Store",
+    color: 0x00ff99,
+    logo: "https://i.imgur.com/9QO4Z9f.png",
+    footer: "💠 MineCom • Secure Auto Delivery",
+    supportUrl: "https://discord.gg/yourserver"
+  }
 };
