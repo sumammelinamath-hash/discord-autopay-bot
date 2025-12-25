416771c4f9373ac5471708a0e5a11ad5e58c6e85
Shreyas <sumammelinamath@gmail.com>
2025-12-25 21:57:56 +0530

    Vouch.js

--- models/Vouch.js
+++ models/Vouch.js
@@ -1,29 +1,10 @@
 const mongoose = require("mongoose");
 
-const VouchSchema = new mongoose.Schema({
-  orderId: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  userId: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5
-  },
-  message: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+const vouchSchema = new mongoose.Schema({
+  orderId: String,
+  userId: String,
+  rating: Number,
+  message: String
 });
 
-module.exports = mongoose.model("Vouch", VouchSchema);
+module.exports = mongoose.model("Vouch", vouchSchema);
