a2fe4eb8d75e3fd3ad0be26b01623609b904605f
Shreyas <sumammelinamath@gmail.com>
2025-12-25 21:54:48 +0530

    config.js

--- config.js
+++ config.js
@@ -3,14 +3,12 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   adminRoleID: process.env.ADMIN_ROLE_ID,
   adminChannelID: process.env.ADMIN_CHANNEL_ID,
-  logChannelID: process.env.LOG_CHANNEL_ID,
   vouchChannelID: process.env.VOUCH_CHANNEL_ID,
-  
   brand: {
-    name: "MineCom Store",
-    color: 0x00ff99,
-    logo: "https://i.imgur.com/9QO4Z9f.png",
-    footer: "💠 MineCom • Secure Auto Delivery",
-    supportUrl: "https://discord.gg/yourserver"
+    name: process.env.BRAND_NAME || "MineCom",
+    color: process.env.BRAND_COLOR || "#00ff99",
+    logo: process.env.BRAND_LOGO || "https://raw.githubusercontent.com/sumammelinamath-hash/MineCom/main/file_0000000063f87209a45937bc7fe7bdea.png",
+    footer: process.env.BRAND_FOOTER || "💠 MineCom • Secure Auto Delivery",
+    supportUrl: process.env.BRAND_SUPPORT_URL || "https://discord.gg/freemcfa"
   }
 };
