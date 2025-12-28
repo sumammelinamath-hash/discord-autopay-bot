0ab11fd5726189e7a58c64a50800d30afea741a8
Shreyas <sumammelinamath@gmail.com>
2025-12-28 22:35:36 +0530

    index.js

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ client.on("interactionCreate", async interaction => {
     if (interaction.isChatInputCommand() && interaction.commandName === "panel") {
       return interaction.reply({
         embeds: [createEmbed().setTitle("<a:cart21:1454879646255681558> Mine Premium Store")
+          .setDescription(
           "<a:zapp:1454474883449749626> **Fast Auto Delivery**\n" + "<a:locked20:1454475603754487819> **Secure & Trusted**\n" + "<a:sos20:1454450996653719643> **24/7 Support**\n"
       )
     ],
