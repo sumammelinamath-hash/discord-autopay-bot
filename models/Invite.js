021e96aaaabbfd68a337bbf5177ef1a9ee452fb9
Shreyas <sumammelinamath@gmail.com>
2025-12-31 19:31:26 +0530

    Invite.js

---
+++ models/Invite.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const inviteSchema = new mongoose.Schema({
+  userId: { type: String, required: true },     // Discord user ID
+  guildId: { type: String, required: true },    // Discord server ID
+  validInvites: { type: Number, default: 0 },   // Valid invites count
+  totalInvites: { type: Number, default: 0 }    // Total invites (optional)
+});
+
+module.exports = mongoose.model("Invites", inviteSchema);
