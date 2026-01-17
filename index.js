a9b6b4a2c3032a83dd561c0700ce43242e51c75d
Shreyas <sumammelinamath@gmail.com>
2026-01-17 19:44:02 +0530

    index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function glowEmbed(title, description, color = GLOW.purple) {
     .setColor(color) // animated glow color
     .setTitle(title)
     .setAuthor({
-      name: "✦<:banana_cart:1462059343456501861> **Legit Cloud | Premium Store✦",
+      name: "✦ <:banana_cart:1462059343456501861> **Legit Cloud | Premium Store✦",
       iconURL: BRAND.logo
     })
     .setDescription(description)
